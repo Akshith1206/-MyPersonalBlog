4cfb39a8efb2283e19f9dc1fa5f3dca037e039a1
Akshith1206 <147523848+Akshith1206@users.noreply.github.com>
2023-10-10 20:20:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// JavaScript code for handling interactivity
+
+// Example: Display an alert when the page loads
+window.onload = function () {
+    alert("Welcome to My Personal Blog!");
+};
+
+// You can add more JavaScript functionality as needed.
